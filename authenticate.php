1996f7e077ba6fa862c6bc24142bead079665fb3
ongzhixian <zhixian@hotmail.com>
2026-02-17 10:53:01 +0800

    remove unncessary pages

--- authenticate.php
+++
@@ -1,9 +0,0 @@
-session_start();
-// Database connection
-// Query to get hashed password
-if (password_verify($_POST['password'], $hashed_password_from_db)) {
-    $_SESSION['loggedin'] = true;
-    header("Location: dashboard.php");
-} else {
-    echo "Invalid credentials";
-}

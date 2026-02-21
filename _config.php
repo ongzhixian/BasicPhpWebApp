ab6cb2e350ef4484e424b87f02a386302bce3e80
ongzhixian <zhixian@hotmail.com>
2026-02-21 14:40:40 +0800

    teest

--- _config.php
+++ _config.php
@@ -35,15 +35,10 @@ spl_autoload_register(function ($class) {
 
     if (strpos($relative_class, 'Includes\\') === 0) {
         $file = $base_dir . 'includes/header.php';
-        if (file_exists($file)) {
-            require_once $file;
-        }
-        return;
+    } else { // Default PSR-4 mapping
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
     }
 
-
-    // Default PSR-4 mapping
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
     if (file_exists($file)) {
         require $file;
     }
